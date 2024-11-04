3763507b3968aca31cf6f5c94f1a4f2eea3e33f2
USER <kmk50009@naver.com>
2024-11-04 16:07:34 +0900

    N개의 숫자의 합

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+
+        // N개의 숫자의 합
+
+        //스캐너 호출
+        Scanner sc = new Scanner(System.in);
+
+        //N값 입력받기
+        int N = sc.nextInt();
+
+        //string으로 입력받기
+        String sNum = sc.next();
+
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for(int i=0; i<cNum.length; i++){
+            sum += cNum[i] - '0'; // 문자를 숫자로 바꿀때
+        }
+
+        System.out.println(sum);
+
+        **/
+        
+
+
+    }
+}
