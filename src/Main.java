824b5c3ddd001b1be6fb28ebedcbf9030e84f269
USER <kmk50009@naver.com>
2024-11-06 18:33:09 +0900

    카드게임 백준2164

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -51,7 +53,21 @@ public class Main {
          System.out.println(s[j] - s[i-1]);
          }
          **/
-
+        /*
+            //카드게임 (백준2164 큐)
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            q.add(i);
+        }
+        while (q.size() > 1){
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+        **/
+        
 
 
 
